a2f4ae441f6ee19f5218c98435d7d1a335e0a9b1
bulbulahmed <ahmedbulbul.cse@gmail.com>
2025-09-12 23:31:53 +0600

    tut 2

--- hello_word/src/main.rs
+++ hello_word/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let x: i32 = 5;
+    println!("x1 = {}, address = {:p}", x, &x);
+
+    let x: i32 = x + 1;
+    println!("x2 = {}, address = {:p}", x, &x);
+
+    let x: i32 = 10;
+    println!("x3 = {}, address = {:p}", x, &x);
+
+    let x = x * 2;
+    println!("x4 = {}, address = {:p}", x, &x);
+
+    let x = "hello";
+    println!("x5 = {}, address = {:p}", x, &x);
+
+
+    const MAX_POINTS: u32 = 100_000;
+    println!("MAX_POINTS = {}", MAX_POINTS); 
 }
